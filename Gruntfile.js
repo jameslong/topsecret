de3514a6330db5daa4479accdbb3b012daf2f48b
James Long <jamesharwoodlong@gmail.com>
2016-04-22 17:17:43 +0100

    Add desktop app and build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,29 @@ module.exports = function (grunt)
         grunt.initConfig({
                 clean: {
                         client: ['client/build'],
+                        desktop: ['desktop/build', 'desktop/src/app/core', 'desktop/src/css', 'desktop/assets'],
                         editor: ['editor/build'],
                         server: ['server/build'],
                         test: ['test/build']
                 },
+                copy: {
+                        desktop: {
+                                files: [
+                                        {expand: true, cwd: 'client/src/app/core', src: ['**'], dest: 'desktop/src/app/core', filter: 'isFile'},
+                                        {expand: true, cwd: 'client/src/css', src: ['**'], dest: 'desktop/src/css', filter: 'isFile'},
+                                        {expand: true, cwd: 'client/assets', src: ['**'], dest: 'desktop/assets', filter: 'isFile'}
+                                ],
+                        },
+                },
                 ts: {
                         client: {
                                 tsconfig: './client/tsconfig.json',
+                                options: {
+                                        compiler: compilerPath
+                                }
+                        },
+                        desktop: {
+                                tsconfig: './desktop/tsconfig.json',
                                 options: {
                                         compiler: compilerPath
                                 }
@@ -46,12 +62,14 @@ module.exports = function (grunt)
         });
 
         grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.loadNpmTasks('grunt-mocha-test');
         grunt.loadNpmTasks('grunt-ts');
 
         grunt.registerTask('client', ['clean:client', 'ts:client']);
+        grunt.registerTask('desktop', ['clean:desktop', 'copy:desktop', 'ts:desktop']);
         grunt.registerTask('editor', ['clean:editor', 'ts:editor']);
         grunt.registerTask('server', ['clean:server', 'ts:server']);
         grunt.registerTask('test', ['clean:test', 'ts:test', 'mochaTest:test']);
-        grunt.registerTask('default', ['client', 'editor', 'server']);
+        grunt.registerTask('default', ['client', 'desktop', 'editor', 'server']);
 }
---
+++ desktop/src/app/main.ts
@@ -0,0 +1,19 @@
+import Data = require('../../../core/src/app/data');
+import Helpers = require('../../../core/src/app/utils/helpers');
+import Main = require('./core/main');
+import State = require('../../../core/src/app/state');
+
+window.onload = () => {
+        const path = '../content';
+        const data = Data.loadNarrativeData(path);
+        const tasks = data.map(narrative => State.addKeyManagers(narrative));
+
+        Promise.all(tasks).then(data =>
+                Helpers.mapFromNameArray(data)
+        ).then(narratives =>
+                Main.init(narratives)
+        ).catch(err => {
+                console.log(err);
+                throw err;
+        });
+};
