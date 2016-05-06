d1645520910b5262c92362e5b6eb789489708da2
James Long <jamesharwoodlong@gmail.com>
2016-05-06 17:48:03 +0100

    Add deploy script to fetch, build, zip and upload game content to s3

---
+++ server/deploy.py
@@ -0,0 +1,37 @@
+#
+# Script to deploy top secret stack with latest game content
+#
+
+import os
+import shutil
+import subprocess
+
+dirname = 'tmp-deploy'
+zipname = 'topsecret'
+zipname_with_ext = zipname + '.zip'
+s3bucketname = 'topsecret-deploy'
+
+# Create temp folder for game assets
+os.makedirs(dirname)
+
+# Clone game content into temp folder
+subprocess.call('git clone --depth 1 git@github.com:jameslong/topsecret-content.git ' + dirname + '/topsecret-content', shell=True)
+subprocess.call('git clone --depth 1 git@github.com:jameslong/topsecret.git ' + dirname + '/topsecret', shell=True)
+
+# Install dependencies
+os.chdir(dirname + '/topsecret')
+subprocess.call('npm install', shell=True)
+subprocess.call('grunt server', shell=True)
+os.chdir('../../')
+
+# Zip content
+shutil.make_archive(zipname, 'zip', dirname)
+
+# Upload to s3 bucket
+subprocess.call('aws s3 cp ' + zipname_with_ext + ' s3://' + s3bucketname, shell=True)
+
+# Delete zipped file
+os.remove(zipname_with_ext)
+
+# Delete temp folder
+shutil.rmtree(dirname)
