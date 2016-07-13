a1f95936aa110bf1e8e0da3d98f5dd7daa055fae
James Long <jamesharwoodlong@gmail.com>
2016-07-13 18:53:00 +0100

    Fix app build

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,11 @@
 /// <reference path="../typings/github-electron/github-electron.d.ts"/>
 
-import Data = require('../core/src/app/data');
-import Helpers = require('../core/src/app/utils/helpers');
+import Data = require('../core/src/data');
+import Helpers = require('../core/src/utils/helpers');
 import Main = require('../browser/src/app/core/main');
 import electron = require('electron');
 const shell = electron.shell;
-import State = require('../core/src/app/state');
+import State = require('../core/src/state');
 
 window.onload = () => {
         const resources = (<any>process)['resourcesPath'];
